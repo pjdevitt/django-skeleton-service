3c80cfdff502eb3d1b3fbb80d37fe48d41181c4c
Philip Devitt <phil@devinebydesign.co.uk>
2020-07-07 19:11:50 +0100

    Some additional useful components.
    Added Jupyter notebook server.
    Added DjangoRestFramework (!)
    Added django-storages

--- django-app/app/settings.py
+++ django-app/app/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'django_q',
-    'django_extensions'
+    'django_extensions',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
